f5f14e159365b45fadfebe184993e99e0ed3ef1c
mowais <mowais1045@gmail.com>
2020-06-02 23:08:44 +0500

    updated app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Created by MOwais
         </a>
       </header>
     </div>
